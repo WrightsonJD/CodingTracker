46afa1c235d42083d2866c93ec8aa3a2941aeadd
Josh Wrightson <joshwrightson@gmail.com>
2025-07-06 01:47:51 -0400

    Keeping 1 message

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -5,11 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, C# Academy! One more modified");
-            Console.WriteLine("Hello, C# Academy! One more modified");
-            Console.WriteLine("Hello, C# Academy! One more modified");
-            Console.WriteLine("Hello, C# Academy! One more modified");
-            Console.WriteLine("Hello, C# Academy! One more modified");
-            Console.WriteLine("Hello, C# Academy! One more modified");
+
         }
     }
 }
